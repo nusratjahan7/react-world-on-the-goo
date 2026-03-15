73807d41ff5593d56acf7e52f45515ff94dbd42e
nusrat <nusratjahan77222@gmail.com>
2026-03-15 21:43:18 +0600

    Use style in react and 3 columns layout and conditional text

--- react-world-on-the-go/src/App.jsx
+++ react-world-on-the-go/src/App.jsx
@@ -1,14 +1,21 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/countries/countries'
 
-function App() {
 
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+    .then(res => res.json())
+    
+
+
+function App() {
   return (
     <>
-     <h1>React World on the go..</h1>
       
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir vai loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
 
      
 
---
+++ react-world-on-the-go/src/components/Country/Country.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Country.css';
+
+const Country = ({country}) => {
+    // console.log(country.area.area);
+
+    const handleVisited = () => {
+        console.log("button clicked");
+    }
+
+    return (
+        <div className='country'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {
+            country.area.area > 300000 ? "Big Country" : "Small Country"
+            }</p>
+            <button onClick={handleVisited} className='button'>Not Visited</button>
+        </div>
+    );
+};
+
+export default Country;
