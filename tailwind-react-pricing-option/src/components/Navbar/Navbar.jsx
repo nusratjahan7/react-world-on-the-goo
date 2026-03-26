5cfbad940ba9a6720deec4dead80997d26e20797
nusrat <nusratjahan77222@gmail.com>
2026-03-26 11:05:00 +0600

    Toggle Menu Items and create pricing data

--- tailwind-react-pricing-option/src/components/Navbar/Navbar.jsx
+++ tailwind-react-pricing-option/src/components/Navbar/Navbar.jsx
@@ -35,14 +35,15 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
     const links =  navgationData.map(route => <Link key={route.id} route = {route}></Link>);
     return (
-        <nav className='flex justify-between mx-10'>
+        <nav className='flex justify-between mx-10 mt-4'>
             <span className='flex' onClick={() => setOpen(!open)}>
              {
                 open ? 
                     <X className='md:hidden'></X> : 
                     <Menu className='md:hidden'></Menu>
              }    
-              <ul className='md:hidden'>
+              <ul className={`md:hidden absolute duration-1000
+                ${open ? 'top-8' : '-top-40'}`}>
                 {links}
               </ul>
             <h3 className='ml-4'>My navbar</h3>
---
+++ tailwind-react-pricing-option/src/components/PricingOptions/PrcingOptions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrcingOptions = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrcingOptions;
