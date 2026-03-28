5814225d8667872c162f042f23282c1405dc14b8
nusrat <nusratjahan77222@gmail.com>
2026-03-28 19:35:37 +0600

    explor axios js chart data prosseing and bar chart

--- tailwind-react-pricing-option/src/App.jsx
+++ tailwind-react-pricing-option/src/App.jsx
@@ -5,9 +5,11 @@ import DaisyNav from './components/DaisyNav/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
 import PricingOptions from './components/PricingOptions/PricingOptions'
 import ResultsChart from './components/ReasultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
 
 const pricingPromise = fetch('pricingData.json').then(res => res.json());
-
+const marksPromise = axios.get('marksData.json');
 
 function App() {
 
@@ -24,6 +26,12 @@ function App() {
       <span className="loading loading-spinner loading-lg"></span>}>
       <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
     </Suspense>
+
+      <Suspense fallback={
+        <span className="loading loading-spinner loading-lg"></span>}>
+      <MarksChart marksPromise={marksPromise}></MarksChart>
+      </Suspense>
+
     <ResultsChart></ResultsChart>
     </main>
 
